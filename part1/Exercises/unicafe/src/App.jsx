58765226e8f1d2b146fe20dd60172356c597bbfc
zarry0 <cs.rgzarrabal@gmail.com>
2024-03-29 22:47:30 -0600

    Added - Exercise 1.6 solution

---
+++ part1/Exercises/unicafe/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      good {good} <br/>
+      neutral {neutral} <br/>
+      bad {bad}
+    </div>
+  );
+}
+
+export default App
