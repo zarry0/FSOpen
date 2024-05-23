75812590b7dc6cba1e66f0a30cdda97cf1ecb03d
zarry0 <cs.rgzarrabal@gmail.com>
2024-05-23 13:23:49 -0600

    Added - problem 2.6 solution

---
+++ part2/Exercises/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    //console.log(e.target)
+    const nameObj = { name : newName };
+    setPersons([...persons, nameObj])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleAdd}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+
+
+export default App
