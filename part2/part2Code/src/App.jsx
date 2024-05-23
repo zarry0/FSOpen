a2c05689c18530c645c84ce8ca4713ec3283fef6
zarry0 <cs.rgzarrabal@gmail.com>
2024-05-23 13:05:39 -0600

    Added - part 2b in-lecture code

--- part2/part2Code/src/App.jsx
+++ part2/part2Code/src/App.jsx
@@ -1,25 +1,82 @@
 /* eslint-disable react/jsx-key */
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
+const App = (props) => {
+
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    console.log("target info", event.target['0'].value);
+    const noteObj = 
+    {
+      id : notes.length+1,
+      content : newNote, //event.target['0'].value,
+      important : Math.random() < 0.5
+    }
+    setNotes([...notes, noteObj])
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log("Event: ", event)
+    console.log("Event target: ", event.target)
+    console.log("Event target value: ", event.target.value)
+    setNewNote(event.target.value)
+  }
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
-      <h1>Notes rendered with a map</h1>
-      <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
-      </ul>
-      <h1>Notes rendered with a map with its own component</h1>
-      <ul>
-        {notes.map(note => <Note key={note.id} note = {note}/>)}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>[2a]Rendering a collection, modules</h1>
+        <h2>Notes</h2>
+        <ul>
+          <li>{props.notes[0].content}</li>
+          <li>{props.notes[1].content}</li>
+          <li>{props.notes[2].content}</li>
+        </ul>
+        <h2>Notes rendered with a map</h2>
+        <ul>
+          {props.notes.map(note => <li key={note.id}>{note.content}</li>)}
+        </ul>
+        <h2>Notes rendered with a map with its own component</h2>
+        <ul>
+          {props.notes.map(note => <Note key={note.id} note = {note}/>)}
+        </ul>
+      </div>
+
+      <div>
+        <h1>[2b]Forms</h1>
+        <div>
+          <h2>Controlled components</h2>
+          <h3>Notes</h3>
+          <ul>
+            {notes.map(note => <Note key={note.id} note={note}/>)}
+          </ul>
+          <form onSubmit={addNote}>
+            <input value={newNote} onChange={handleNoteChange}/>
+            <button type='submit'>save</button>
+          </form>
+        </div>
+        <div>
+          <h2>Filtering Notes</h2>
+          <h3>Notes</h3>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+          <ul>
+            {notesToShow.map(note => <Note key={note.id} note={note}/>)}
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
+
 
 export default App
