12b0ef4eb8281436f809e6d4eb6c745036cb6bc8
RW1844164 <rodrigo.weller@cerner.com>
2024-07-16 20:59:19 -0600

    Added - section 2d in-lecture code

--- part2/part2Code/src/App.jsx
+++ part2/part2Code/src/App.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from 'react'
 import Note from './components/Note'
+import ImportantNote from './components/ImportantNote.jsx'
+import noteService from './services/notes.js'
 import axios from 'axios'
 
 const App = (props) => {
@@ -11,21 +13,30 @@ const App = (props) => {
 
   const [notesEffect, setNotesEffect] = useState([]);
   const [finishFetch, setFinishFetch] = useState(false);
+
   useEffect(() => {
     console.log('Effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(
-        response => {
-        console.log('promise fulfilled')
-        setNotesEffect(response.data)
+    // axios
+    //   .get('http://localhost:3001/notes')
+    //   .then(
+    //     response => {
+    //     console.log('promise fulfilled')
+    //     setNotesEffect(response.data)
+    //     setFinishFetch(true)
+    //   },
+    //     () => setFinishFetch(false))
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotesEffect(initialNotes);
         setFinishFetch(true)
       },
-        () => setFinishFetch(false))
+      () => setFinishFetch(false));
   }, [])
   console.log('render', notesEffect.length, 'notes')
   
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow2d = showAll ? notesEffect : notesEffect.filter(note => note.important);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -47,9 +58,58 @@ const App = (props) => {
     console.log("Event target value: ", event.target.value)
     setNewNote(event.target.value)
   }
+
+  const addNoteToServer = (event) => {
+    event.preventDefault();
+    console.log("buton pressed, sending to server...");
+    const noteObj = {
+      id : `${notesEffect.length + 1}`,
+      content : newNote,
+      important : Math.random() < 0.5
+    };
+
+    // axios
+    //   .post('http://localhost:3001/notes', noteObj)
+    //   .then(response => {
+    //     console.log(response)
+    //     setNotesEffect(notesEffect.concat(response.data))
+    //     setNewNote('')
+    //   })
+
+    noteService.create(noteObj)
+      .then(
+        createdNote => {
+        console.log(createdNote)
+        setNotesEffect(notesEffect.concat(createdNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = (id) => {
+    console.log('importance of ' + id + ' needs to be toggled')
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notesEffect.find(n => n.id === id);
+    const updatedNote = {...note, important : !note.important};
+
+    // axios.put(url, updatedNote)
+    //   .then(response => {
+    //     setNotesEffect(notesEffect.map(n => n.id !== id ? n : response.data));
+    //   });
+
+    noteService.update(id, updatedNote)
+      .then(toggledNote => {
+        setNotesEffect(notesEffect.map(n => n.id !== id ? n : toggledNote));
+      })
+      .catch(error => {
+        alert(`the note "${note.content}" was already deleted from server`);
+        setNotesEffect(notesEffect.filter(n => n.id !== id))        
+      })
+  };
+  
+
   return (
     <>
-      <div>
+      <div> {/* 2a */}
         <h1>[2a]Rendering a collection, modules</h1>
         <h2>Notes</h2>
         <ul>
@@ -67,7 +127,7 @@ const App = (props) => {
         </ul>
       </div>
 
-      <div>
+      <div> {/* 2b */}
         <h1>[2b]Forms</h1>
         <div>
           <h2>Controlled components</h2>
@@ -92,7 +152,7 @@ const App = (props) => {
         </div>
       </div>
 
-      <div>
+      <div> {/* 2c */}
         <h1>[2c]Getting data from server</h1>
         <h2>Notes (Fetched from json-server)</h2>
         {finishFetch && 
@@ -103,6 +163,23 @@ const App = (props) => {
         </ul> 
         }
         
+      </div>
+
+      <div> {/* 2d */}
+        <h1>[2d] Altering data in server</h1>
+        <h2>Notes (fetched from json server)</h2>
+        {finishFetch &&
+          <>
+            <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
+            <ul>
+              {notesToShow2d.map(note => <ImportantNote note={note} toggleImportance={() => toggleImportanceOf(note.id)} key={note.id}/>)}
+            </ul>
+            <form onSubmit={addNoteToServer}>
+              <input value={newNote} onChange={handleNoteChange}/>
+              <button type='submit'>add new note</button>
+            </form>
+          </>
+        }
       </div>
     </>
   )
